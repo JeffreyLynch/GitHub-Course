545a97f49d0d80027719a10c8b171e21ca729e1e
Jeff Lynch <jeffreymlynch@gmail.com>
2019-10-07 20:12:18 -0400

    Added Addressables

---
+++ Assets/AddressableTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class AddressableTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(transform.GetSiblingIndex());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/LoadAddressable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class LoadAddressable : MonoBehaviour
+{
+    [SerializeField] private List<AssetReference> assetReferences;
+    // Start is called before the first frame update
+
+    private readonly Dictionary<AssetReference, AsyncOperationHandle<GameObject>> asyncOperationHandles = new Dictionary<AssetReference, AsyncOperationHandle<GameObject>>();
+    
+    private void Spawn()
+    {
+        var assetRef = assetReferences[0];
+        if (assetRef.RuntimeKeyIsValid() == false)
+        {
+            throw new InvalidKeyException("You have an invalid asset ref key" + assetRef.RuntimeKey);
+            
+        }
+
+        if (asyncOperationHandles.ContainsKey(assetRef))
+        {
+            if (asyncOperationHandles[assetRef].IsDone)
+            {
+                SpawnFromLoadedRef(assetRef, Vector3.zero);
+            }
+        }
+
+        LoadAndSpawn(assetRef);
+    }
+
+    private void LoadAndSpawn(AssetReference assetRef)
+    {
+        var op = Addressables.LoadAssetAsync<GameObject>(assetRef);
+      
+    }
+
+    private void SpawnFromLoadedRef(AssetReference assetRef, Vector3 zero)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
